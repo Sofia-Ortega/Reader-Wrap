05e986332766933535ad20d8488296e91f68258d
Sofia Ortega <sofia.ortega@aa.com>
2024-12-11 02:59:26 -0600

    fixed types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,7 +6,7 @@ export type PageType =
   | "Test"
   | "Bookshelf";
 
-export interface IBook {
+interface IBookBase {
   bookId: string;
   title: string;
   author: string;
@@ -14,21 +14,35 @@ export interface IBook {
   additionalAuthors: string;
   isbn: string;
   isbn13: string;
-  myRating: number;
-  averageRating: number;
   publisher: string;
   binding: string;
-  numberOfPages: number;
-  yearPublished: number;
-  originalPublicationYear: number;
-  dateRead: Date | null;
-  dateAdded: Date | null;
   bookshelves: string;
   bookshelvesWithPositions: string;
   exclusiveShelf: string;
   myReview: string;
   spoiler: string;
   privateNotes: string;
+}
+export interface IBook extends IBookBase {
+  myRating: number;
+  averageRating: number;
+  numberOfPages: number;
+  yearPublished: number;
+  originalPublicationYear: number;
+  dateRead: Date | null;
+  dateAdded: Date | null;
   readCount: number;
   ownedCopies: number;
 }
+
+export interface IBookLocalStorage extends IBookBase {
+  myRating: string;
+  averageRating: string;
+  numberOfPages: string;
+  yearPublished: string;
+  originalPublicationYear: string;
+  dateRead: string | null;
+  dateAdded: string | null;
+  readCount: string;
+  ownedCopies: string;
+} 
