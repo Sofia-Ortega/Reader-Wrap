8fadc63865d808882f25cca08cefa5a1d57943e7
Sofia Ortega <s.ortega@tamu.edu>
2025-01-04 00:53:55 -0600

    added dummy data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import BookmarkButton from "../components/global/BookmarkButton";
 import { useContext } from "react";
 import { PageContext } from "../App";
 import { IBook } from "../utils/types";
-import { readBooksFromLocalStorage } from "../utils/bookStatsUtil";
+import { dummyBooks } from "../assets/data/dummydata";
 
 const ButtonWrapper = styled.div`
   display: flex;
@@ -19,25 +19,14 @@ interface Props {
 export default function Home({ handleSetBooks }: Props) {
   const setShowPage = useContext(PageContext);
 
-  const handleYourWrapClick = () => {
-    const books = readBooksFromLocalStorage();
-    if (books == null) {
-      console.log("no books found");
-      return;
-    }
-
-    handleSetBooks(books);
-    setShowPage("Stats");
+  const handlePreview = () => {
+    handleSetBooks(dummyBooks);
+    setShowPage("Wrap");
   };
 
   return (
     <div>
       <ButtonWrapper>
-        <BookmarkButton
-          primaryText="Your Wrap"
-          secondaryText="See your personalized results"
-          onClick={handleYourWrapClick}
-        />
         <BookmarkButton
           primaryText="Start"
           secondaryText="See your personalized results"
@@ -47,7 +36,7 @@ export default function Home({ handleSetBooks }: Props) {
           primaryText="Preview"
           secondaryText="Preview sample results"
           variation="secondary"
-          onClick={() => setShowPage("Wrap")}
+          onClick={handlePreview}
         />
       </ButtonWrapper>
     </div>
