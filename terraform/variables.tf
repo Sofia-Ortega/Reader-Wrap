7b62b1f03636ae2029044a34f51d4130135c6fec
Sofia-Ortega <s.ortega@tamu.edu>
2025-11-29 02:29:54 -0600

    deployed ec2 container with simple flask app

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+
+variable "availability_zone" {
+  description = "Availability zone of resources"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "Public key to ssh into my ec2 container"
+  type        = string
+}
+
+variable "instance_ami" {
+  description = "ID of AMI used for EC2"
+  type        = string
+
+}
