359e5ed674c2e3abb2fd69dd831034dd61433402
Sofia-Ortega <s.ortega@tamu.edu>
2025-12-17 23:20:02 -0600

    updated certificate ec2

--- terraform/backend/certificate.tf
+++ terraform/backend/certificate.tf
@@ -1,10 +1,31 @@
 
-# resource "aws_acm_certificate" "api_cert" {
-#   domain_name = "api.readerwrap.com"
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "api_cert" {
+  domain_name = "api.readerwrap.com"
+  validation_method = "DNS"
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
+resource "aws_route53_record" "api_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api_cert.domain_validation_options :
+    dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
+    }
+  }
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "api_cert_validation" {
+  certificate_arn         = aws_acm_certificate.api_cert.arn
+  validation_record_fqdns = [for r in aws_route53_record.api_cert_validation : r.fqdn]
+}
